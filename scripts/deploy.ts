f548f225124993fd6792e46bd3a8a7233160188f
NuttakitDW <nuttakit@roostoo.com>
2022-08-27 14:15:11 +0700

    init

---
+++ scripts/deploy.ts
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const MazkGang = await ethers.getContractFactory("MazkGang");
+  const mazk = await MazkGang.deploy(0, 1, 2, 3, 4, 10000, 10000, 10000);
+
+  await mazk.deployed();
+
+  console.log("MazkGang deployed to:", mazk.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
