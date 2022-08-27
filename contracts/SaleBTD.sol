50a61cc30ea745d79ed6ab107ab6731b8c4177cc
NuttakitDW <nuttakit@roostoo.com>
2022-08-27 15:46:29 +0700

    typo

--- contracts/SaleBTD.sol
+++ contracts/SaleBTD.sol
@@ -73,7 +73,7 @@ contract SaleBTD is Ownable, ReentrancyGuard {
         require(block.timestamp >= privateTime, "Private mint is not open.");
         require(getTotalSupply() + _amount <= getMaxSupply(), "Over supply amount.");
         require(privateUserMintedAmount(msg.sender) + _amount <= _maxAmount, "Exceed Whitelist Limit");
-        require(MerkleProof.verify(_proof, merkleRoot, keccak256(abi.encodePacked(msg.sender, _amount))), "Unauthorized user.");
+        require(MerkleProof.verify(_proof, merkleRoot, keccak256(abi.encodePacked(msg.sender, _maxAmount))), "Unauthorized user.");
         require(privatePrice * _amount <= msg.value, "Ether value sent is not correct");
 
         _privateUserMintedAmount[msg.sender] += _amount;
